d70f1c12562e910006ce8cbd79c0577fbb11a535
JuhoYlostalo <juho.ylostalo@gmail.com>
2025-09-01 14:17:21 +0300

    part 5 valmis

--- Server/data.sql
+++ Server/data.sql
@@ -4,11 +4,11 @@ create table task (
 );
 
 insert into task (description) values
-("Compelete the project documentation"),
-("Review the code changes"),
-("Prepare for the team meeting"),
-("Update the project timeline"),
-("Test the new features"),
-("Fix the reported bugs"),
-("Deploy the aplication to production"),
-("Conduct a code review with peers");
+('Compelete the project documentation'),
+('Review the code changes'),
+('Prepare for the team meeting'),
+('Update the project timeline'),
+('Test the new features'),
+('Fix the reported bugs'),
+('Deploy the aplication to production'),
+('Conduct a code review with peers');
--- Server/index.js
+++ Server/index.js
@@ -1,79 +1,26 @@
 import express from "express"
 import cors from "cors"
-import pkg from "pg"
+import todoRouter from "./routes/todoRouter.js"
 
-const port = 3001
-const {Pool} = pkg
 
-const openDb = () => {
-    const pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'ToDo',
-        password: '1234',
-        port: 5432
-    })
-    return pool
-}
+const port = process.env.PORT
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
-app.get("/", (req,res) => {
-    const pool = openDb()
-
-    pool.query('SELECT * FROM task', (err,result) => {
-        if (err){
-            return res.status(500).json({err: err.message})
+app.use("/", todoRouter)
+app.listen(port)
+app.use((err,req,res,next)=> {
+    const statusCode = err.status || 500
+    res.status(statusCode).json({
+        err: {
+            messsage: err.messsage,
+            status: statusCode
         }
-
-        res.status(200).json(result.rows)
     })
 })
 
-app.post("/create", (req,res) => {
-    const pool = openDb()
-    const {task} = req.body
 
-    if (!task) {
-        return res.status(400).json({error:"Task is required"})
-    }
 
-    pool.query('INSERT INTO TASK (description) VALUES ($1) RETURNING *', [task.description],
-        (err,result) => {
-            if (err) {
-                return res.status(500).json({error: err.message})
-            }
-
-            res.status(201).json({id: result.rows[0].id, description: task.description})
-        }
-    )
-})
-
-app.delete("/delete/:id", (req,res) => {
-    const pool = openDb()
-    const {id} = req.params
-
-    console.log(`deleting task with id: ${id}`)
-
-    pool.query('DELETE FROM TASK WHERE id = $1', [id],
-        (err,result) => {
-            if (err) {
-                console.log(err.message)
-                return res.status(500).json({error: err.message})
-            }
-
-            if (result.rowCount === 0) {
-                return res.status(404).json({error: "Task not found"})
-            }
-            return res.status(200).json({id:id})
-        }
-    )
-})
-
-app.listen(port, () => {
-    console.log(`server running ${port}`)
-})
 
